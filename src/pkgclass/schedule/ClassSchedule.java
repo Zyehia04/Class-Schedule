5f4dbb0f0a62413122ae73beee5415c3e4ad89a2
21399 <21399@MININT-MPKMA8T.columbushs.com>
2019-09-09 09:22:48 -0400

    Done

---
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 21399
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String name = "Zein";
+       int graduate = 2021;
+       // Classes
+       String class1 = "American History";
+       String class2 = "AP Computer Science";
+       String class3 = "Physics";
+       String class4 = "College Algebra and Trigonometry";
+       String class5 = "Global Studies";
+       String class6 = "English";
+       String class7 = "Graphic Animation/Visual Effects";
+       // Teachers
+       String teacher1 = "Mr. Benitez";
+       String teacher2 = "Mr. Larrubia";
+       String teacher3 = "Mr. Ferguson";
+       String teacher5 = "Mr. Gonzalez";
+       String teacher6 = "Mrs. Pina";
+       String teacher7 = "Mr. Delgado";
+       
+       // I have Mr. Ferguson for period 3 and 4 so I only made one variable for both
+       
+    System.out.println("+-----------------------------------------------------+"); 
+    System.out.println("| 1 | " + class1 + " | " + teacher1 + "                  |" );
+    System.out.println("| 2 | " + class2 + " | " + teacher2 + "              |"  );
+    System.out.println("| 3 | " + class3 + " | " + teacher3 + "                          |"  );
+    System.out.println("| 4 | " + class4 + " | " + teacher3 + " |"  );
+    System.out.println("| 5 | " + class5 + " | " + teacher5 + "                   |"  );
+    System.out.println("| 6 | " + class6 + " | " + teacher6 + "                             |"  );
+    System.out.println("| 7 | " + class7 + " | " + teacher7 + "  |"  );
+    System.out.println("+-----------------------------------------------------+");
+    
+    }
+    
+}
